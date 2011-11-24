466fd2e0a53d648a787700f7da3e399e74132db2
Grischa <grischa@gmail.com>
2011-11-24 12:56:21 +0100

    added download urls

--- closure.sh
+++ closure.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 java -jar ~/.emacs.d/compiler-latest.jar --warning_level VERBOSE --js $1 --js_output_file /dev/null
+# get this at http://code.google.com/closure/compiler/ and http://closure-compiler.googlecode.com/files/compiler-latest.zip
