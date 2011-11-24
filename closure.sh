9694a8792e273b9222903dea31418d142316e95a
Dr. Grischa R. Meyer <grischa@gmail.com>
2011-11-24 12:47:44 +0100

    use latest closure compiler

--- closure.sh
+++ closure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar ~/.emacs.d/compiler.jar --warning_level VERBOSE --js $1 --js_output_file /dev/null
+java -jar ~/.emacs.d/compiler-latest.jar --warning_level VERBOSE --js $1 --js_output_file /dev/null
